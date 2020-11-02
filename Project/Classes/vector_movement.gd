8941c7946b43ec032ffa2a23e3d22bebd697fda2
Jonas <jonas_munz@outlook.com>
2020-11-02 02:20:11 -0600

    Created Vector Movement. Idea is to have all moving Kinematic Bodies follow a vector called 'forward'. My manipulating the forward vector, the bodies will move to a certain direction

---
+++ Project/Classes/vector_movement.gd
@@ -0,0 +1,16 @@
+extends Node
+
+class_name VectorMovement
+
+var forward: Vector2	# vector that entity follows
+
+func _ready():
+	forward = Vector2(1,0)	# Face right on start
+
+func interpolate_forward(direction: Vector2, p_forward: Vector2) -> Vector2:
+	var new_forward = p_forward.linear_interpolate(direction, 1.0)
+	
+	return new_forward
+	
+func update_forward(direction: Vector2):
+	forward = interpolate_forward(direction, forward)
---
+++ test/unit/VectorMovement/test_vector_movement.gd
@@ -0,0 +1,29 @@
+extends "res://addons/gut/test.gd"
+
+var VMovement = preload("res://Project/Classes/vector_movement.gd")
+
+func test_forward_interpolation():
+	var vector_movement = VMovement.new()
+	var t_forward = Vector2(1,0)
+	
+	var t_direction_one = Vector2(0,1)
+	var t_direction_two = Vector2(1,0)
+	var t_direction_three = Vector2(1,1)
+	
+	var expected_one = Vector2(0,1)
+	var expected_two = Vector2(1,0)
+	var expected_three = Vector2(1,1)
+	
+	assert_eq(vector_movement.interpolate_forward(t_direction_one, t_forward), expected_one)
+	assert_eq(vector_movement.interpolate_forward(t_direction_two, t_forward), expected_two)
+	assert_eq(vector_movement.interpolate_forward(t_direction_three, t_forward), expected_three)
+	
+func test_forward_update():
+	var vector_movement = VMovement.new()
+	
+	var t_direction = Vector2(0,1)
+	
+	var expected = Vector2(0,1)
+	
+	vector_movement.update_forward(t_direction)
+	assert_eq(vector_movement.forward, expected)
