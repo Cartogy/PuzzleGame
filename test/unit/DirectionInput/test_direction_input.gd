ade11f6aedc967beaf3a7716f1e5c9a6aee274f2
Jonas <jonas_munz@outlook.com>
2020-11-02 03:36:05 -0600

    Change Instance to instance. Null error should be fixed

--- test/unit/DirectionInput/test_direction_input.gd
+++ test/unit/DirectionInput/test_direction_input.gd
@@ -4,32 +4,52 @@ var DInput = load("res://Project/Classes/DirectionInput/direction_input.gd")
 
 func test_direction_input_down():
 	var d_input = DInput.new()
-	Input.action_press("ui_down")
+	var k = InputEventKey.new()
+	k.scancode = KEY_DOWN
+	k.pressed = true
+	Input.parse_input_event(k)
 	var value = d_input.get_input()
-	var expected = Vector2(0, -1)
+	var expected = Vector2(0, 1).normalized()
 	
 	assert_eq(value, expected)
 	
 func test_direction_input_up():
 	var d_input = DInput.new()
-	Input.action_press("ui_up")
+	
+	var k = InputEventKey.new()
+	k.scancode = KEY_UP
+	k.pressed = true
+	Input.parse_input_event(k)
+	
 	var value = d_input.get_input()
-	var expected = Vector2(0, 1)
+	var expected = Vector2(0, -1).normalized()
 	
 	assert_eq(value, expected)
 	
+	d_input.queue_free()
+	
 func test_direction_input_right():
 	var d_input = DInput.new()
-	Input.action_press("ui_right")
+	
+	var k = InputEventKey.new()
+	k.scancode = KEY_RIGHT
+	k.pressed = true
+	Input.parse_input_event(k)
+	
 	var value = d_input.get_input()
-	var expected = Vector2(1,0)
+	var expected = Vector2(1,0).normalized()
 	
 	assert_eq(value, expected)
 	
 func test_direction_input_left():
 	var d_input = DInput.new()
-	Input.action_press("ui_left")
+	
+	var k = InputEventKey.new()
+	k.scancode = KEY_LEFT
+	k.pressed = true
+	Input.parse_input_event(k)
+	
 	var value = d_input.get_input()
-	var expected = Vector2(-1,0)
+	var expected = Vector2(-1,0).normalized()
 	
 	assert_eq(value, expected)
--- test/unit/VectorMovement/test_vector_movement.gd
+++ test/unit/VectorMovement/test_vector_movement.gd
@@ -1,9 +1,9 @@
 extends "res://addons/gut/test.gd"
 
-var VMovement = load("res://Project/Classes/VectorMovement/VectorMove.tscn")
+var VMovement = preload("res://Project/Classes/VectorMovement/VectorMove.tscn")
 
 func test_forward_interpolation():
-	var vector_movement = VMovement.Instance()
+	var vector_movement = VMovement.instance()
 	var t_forward = Vector2(1,0)
 	
 	var t_direction_one = Vector2(0,1)
@@ -19,7 +19,7 @@ func test_forward_interpolation():
 	assert_eq(vector_movement.interpolate_forward(t_direction_three, t_forward), expected_three)
 	
 func test_forward_update():
-	var vector_movement = VMovement.Instance()
+	var vector_movement = VMovement.instance()
 	
 	var t_direction = Vector2(0,1)
 	
