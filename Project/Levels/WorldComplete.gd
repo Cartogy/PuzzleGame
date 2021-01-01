4533158302c6c9f04e5e5fd9e48a381a128f8fc5
ldrg <ldrg@users.noreply.github.com>
2021-01-01 23:25:10 +0000

    Amended Collisions and added world completion checks
    Added label to collision layers.
    Moved tilemap to it's own collision layer.
    Moved WorldComplete to it's own collision layer.
    Amended collision mask for WorldComplete.
    Added WorldComplete check to allow levels to require either both Duck and Ducklings to be at the finish to complete the level or have either of them be at the finish to complete the level.
    Added a new world using stage completion with either duck or ducklings.

--- Project/Levels/WorldComplete.gd
+++ Project/Levels/WorldComplete.gd
@@ -2,9 +2,15 @@ extends Area2D
 
 signal level_complete(next_level)
 
+enum Pass_Critera { BOTH, EITHER}
+export (Pass_Critera) var pass_critera = 0
+
 export (String, FILE, "*.tscn") var next_world
 
 var game
+var credits = "res://Project/GameScenes/EndCredits/EndCredits.tscn"
+var duckPass = false
+var ducklingPass = false
 
 func _ready():
 	game = null
@@ -12,13 +18,35 @@ func _ready():
 		self.connect("level_complete", game, "_on_World_Complete")
 
 func _physics_process(delta):
+	_on_World_Complete()
+
+func duckAndDucklingPass():
+	if duckPass and ducklingPass:
+		change_world()
+
+func duckOrDucklingPass():
+	if duckPass or ducklingPass:
+		change_world()
+	
+func change_world():
+	if next_world:
+		if game:
+			emit_signal("level_complete", next_world)
+		else:
+			get_tree().change_scene(next_world)
+	else:
+		get_tree().change_scene(credits)
+
+func _on_World_Complete():
+	duckPass = false
+	ducklingPass = false
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Duck":
-			if next_world:
-				if game:
-					emit_signal("level_complete", next_world)
-				else:
-					get_tree().change_scene(next_world)
-#			else: #Go back to menu?
-#				pass
+			duckPass = true
+		if body.get_parent().name == "Flock":
+			ducklingPass = true
+	if pass_critera == 0:
+		duckAndDucklingPass()
+	else:
+		duckOrDucklingPass()
